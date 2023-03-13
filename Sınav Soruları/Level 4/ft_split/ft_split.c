c45b6a389bd921ef512e9a19743b81dbeed2bcd7
Ahmet Mert Kalaman <ahkalama@k2m17s03.42kocaeli.com.tr>
2023-03-13 17:32:59 +0300

    bitti

--- Sınav Soruları/Level 4/ft_split/ft_split.c
+++ Sınav Soruları/Level 4/ft_split/ft_split.c
@@ -1,72 +1,40 @@
-#include <stdlib.h>
+#include<stdlib.h>
 
-int	ft_wordlen(char *str)
+char    **ft_split(char *str)
 {
-	int i = 0;
-
-	while (str[i] != '\0' && str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
-		++i;
-	return (i);
+    char **tab;
+    int index = 0;
+    int tab1_i = 0;
+    int tab2_i = 0;
+    tab = (char**)malloc(sizeof(char *) * 1000);
+    while (str[index] == ' ' || str[index] == '\t' || str[index] == '\n')
+          index++;
+    while(str[index])
+    {
+      if (str[index] > 32)
+      {
+        tab[tab1_i] = (char*)malloc(sizeof(char)*1000);
+        tab2_i = 0;
+        while (str[index] > 32)
+        {
+            tab[tab1_i][tab2_i] = str[index];
+            index++;
+            tab2_i++;
+        }
+        tab[tab1_i][tab2_i] = '\0';
+        tab1_i++;
+      }
+      else
+        index++;
+    }
+    return tab;
 }
-
-char	*word_dupe(char *str)
-{
-	int i = 0;
-	int len = ft_wordlen(str);
-	char *word = malloc(sizeof(char) * (len + 1));
-	
-	word[len] = '\0';
-	while (i < len)
-	{
-		word[i] = str[i];
-		++i;
-	}
-	return (word);
-}
-
-void	fill_words(char **array, char *str)
-{
-	int word_index = 0;
-	
-	while (*str == ' ' || *str == '\t' || *str == '\n')
-		++str;
-	while (*str != '\0')
-	{
-		array[word_index] = word_dupe(str);
-		++word_index;
-		while (*str != '\0' && *str != ' ' && *str != '\t' && *str != '\n')
-			++str;
-		while (*str == ' ' || *str == '\t' || *str == '\n')
-			++str;
-	}
-}
-
-int		count_words(char *str)
-{
-	int num_words = 0;
-	
-	while (*str == ' ' || *str == '\t' || *str == '\n')
-		++str;
-	while (*str != '\0')
-	{
-		++num_words;
-		while (*str != '\0' && *str != ' ' && *str != '\t' && *str != '\n')
-			++str;
-		while (*str == ' ' || *str == '\t' || *str == '\n')
-			++str;
-	}
-	return (num_words);
-}
-
-char	**ft_split(char *str)
-{
-	int		num_words;
-	char	**array;
-	
-	num_words = count_words(str);
-	array = malloc(sizeof(char *) * (num_words + 1));
-	
-	array[num_words] = 0;
-	fill_words(array, str);
-	return (array);
-}
+// #include <stdio.h>
+// int main()
+// {
+//     char str[] = "ahmet   mert  kalaman";
+//     char **p = ft_split(str);
+//     printf("%s",p[0]);
+//     printf("%s",p[1]);
+//     printf("%s",p[2]);
+// }
