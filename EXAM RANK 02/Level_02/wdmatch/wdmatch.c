bc9a06ee140f74ce5b309f239461c7ec6889fce0
ahkalama <116187665+ahkalama@users.noreply.github.com>
2023-05-25 11:45:03 +0300

    Update wdmatch.c

--- EXAM RANK 02/Level_02/wdmatch/wdmatch.c
+++ EXAM RANK 02/Level_02/wdmatch/wdmatch.c
@@ -2,53 +2,25 @@
 
 void	ft_putstr(char *str)
 {
-	int i;
-
-	i = 0;
-	while (str[i] != '\0')
+	int i = 0;
+	while(str[i])
 	{
 		write(1, &str[i], 1);
 		i++;
 	}
 }
 
-int		ft_strlen(char *str)
+int	main(int ac, char **av)
 {
-	int i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-int		main(int ac, char **av)
-{
-	int i;
-	int i2;
-	int wdlen;
-
-	i = 0;
-	i2 = 0;
-	wdlen = 0;
-	if (ac == 3)
+	int i = 0;
+	int j = 0;
+	if(ac == 3)
 	{
-		while (av[1][i] != '\0')
-		{
-			while (av[2][i2] != '\0')
-			{
-				if (av[1][i] == av[2][i2])
-				{
-					wdlen++;
-					break ;
-				}
-				i2++;
-			}
-			i++;
-		}
-		if (wdlen == ft_strlen(av[1]))
-			ft_putstr(av[1]);
+		while(av[2][j])
+			if(av[2][j++] == av[1][i])
+				i++;
+			if(!av[1][i])
+				ft_putstr(av[1]);
 	}
 	write(1, "\n", 1);
-	return (0);
 }
