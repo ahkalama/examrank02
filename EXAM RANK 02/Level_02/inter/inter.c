c7e30bb888a75ac80c94cecece66f6c7a2eb7b35
ahkalama <116187665+ahkalama@users.noreply.github.com>
2023-06-02 15:26:11 +0300

    Update inter.c

--- EXAM RANK 02/Level_02/inter/inter.c
+++ EXAM RANK 02/Level_02/inter/inter.c
@@ -2,28 +2,25 @@
 
 int	iter(char *str, char c, int len)
 {
-	int	i = 0;
-
-	while (str[i] && (i < len || len == -1))
-		if (str[i++] == c)
-			return (1);
-	return (0);
+	int i = 0;
+	while(str[i] && (i < len || len == -1))
+		if(str[i++] == c)
+			return 1;
+		return 0;
 }
 
-int	main(int argc, char *argv[])
+int	main(int ac, char **av)
 {
-	int	i;
-
-	if (argc == 3)
+	int i = 0;
+	if(ac == 3)
 	{
-		i = 0;
-		while (argv[1][i])
+		while(av[1][i])
 		{
-			if (!iter(argv[1], argv[1][i], i) && iter(argv[2], argv[1][i], -1))
-				write(1, &argv[1][i], 1);
-			i += 1;
+			if(!iter(av[1], av[1][i], i) && iter(av[2], av[1][i], -1))
+				write(1, &av[1][i], 1);
+			i++;
 		}
 	}
 	write(1, "\n", 1);
-	return (0);
+	return 0;
 }
