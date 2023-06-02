4b8dedf2b752cfc48c5de1215dd55c375a595b6e
ahkalama <116187665+ahkalama@users.noreply.github.com>
2023-06-02 12:41:46 +0300

    Update union.c

--- EXAM RANK 02/Level_02/union/union.c
+++ EXAM RANK 02/Level_02/union/union.c
@@ -1,43 +1,38 @@
 #include <unistd.h>
 
-int check(int c, char *str, int index)
+int	check(int c, char *str, int index)
 {
 	int i = 0;
-	
 	while(i < index)
 	{
-		if (str[i] == c)
+		if(str[i] == c)
 			return 0;
-		i++;
+			i++;
 	}
-	return  1;
+	return 1;
 }
 
-int main(int argc, char **argv)
-{	
+int	main(int ac, char **av)
+{
 	int i = 0;
 	int j = 0;
-	int k = 0;
-	
-	if (argc == 3)
+	if(ac == 3)
 	{
-		while(argv[1][i] != '\0')
-		{
+		while(av[1][i])
 			i++;
-		}
-		while(argv[2][j] != '\0')
+		while(av[2][j])
 		{
-			argv[1][i] = argv[2][j];
+			av[1][i] = av[2][j];
 			i++;
 			j++;
 		}
-		i--;
-		while(k <= i)
+		i = 0;
+		while(av[1][i])
 		{
-			if(check(argv[1][k], argv[1], k) == 1) 
-				write (1, &argv[1][k], 1); 
-			k++;
+			if(check(av[1][i], av[1], i) == 1)
+				write(1, &av[1][i], 1);
+				i++;
 		}
 	}
-	write (1, "\n", 1);
+	write(1, "\n", 1);
 }
