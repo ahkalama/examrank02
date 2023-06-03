50e27cf33023ea07474a3d4b3dda9d73981376bc
ahkalama <116187665+ahkalama@users.noreply.github.com>
2023-06-03 15:26:03 +0300

    amertkupdateversion2.1

--- EXAM RANK 02/Level_02/last_word/last_word.c
+++ EXAM RANK 02/Level_02/last_word/last_word.c
@@ -3,20 +3,18 @@
 int	main(int ac, char **av)
 {
 	int i = 0;
-	int j = 0;
 	if(ac == 2)
 	{
 		while(av[1][i])
-		{
-			if(av[1][i] == ' ' && av[1][i + 1] >= 33 && av[1][i + 1] <= 126)
-				j = i + 1;
 			i++;
-		}
-		while(av[1][j])
+		i--;
+		while(av[1][i] != ' ' && av[1][i] != '\t' && av[1][i])
+			i--;
+		i++;
+		while(av[1][i] && av[1][i] != ' ' && av[1][i] != '\t')
 		{
-			if(av[1][j] >= 33 && av[1][j] <= 126)
-				write(1, &av[1][j], 1);
-			j++;
+			write(1, &av[1][i], 1);
+			i++;
 		}
 	}
 	write(1, "\n", 1);
