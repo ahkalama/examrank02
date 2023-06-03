c815bc45550111cae0470b91cc19aac16f67f42f
ahkalama <116187665+ahkalama@users.noreply.github.com>
2023-06-03 15:02:34 +0300

    amertkupdateversion2.1

---
+++ EXAM RANK 02/Level_02/inter/amertknıninteri.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+int	check(int c, char *str, int index)
+{
+	int i = 0;
+	while(i < index)
+	{
+		if(str[i] == c)
+			return 0;
+		i++;
+	}
+	return 1;
+}
+
+int	main(int ac, char **av)
+{
+	int i = 0;
+	if(ac == 3)
+	{
+		while(av[1][i])
+		{
+			if(av[1][i] && (check(av[1][i], av[1], i)))
+				write(1, &av[1][i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+}
