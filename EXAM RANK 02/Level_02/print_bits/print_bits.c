9eecac892236c5bad53bc72098ff8a68a258a45b
ahkalama <116187665+ahkalama@users.noreply.github.com>
2023-06-03 17:10:04 +0300

    amertkupdateversion2.2

--- EXAM RANK 02/Level_02/print_bits/print_bits.c
+++ EXAM RANK 02/Level_02/print_bits/print_bits.c
@@ -1,22 +1,13 @@
 #include <unistd.h>
 
-void	print_bits(unsigned char octet)
+void print_bits(unsigned char octet)
 {
-	int i;
+	int	i = 8;
+	unsigned char 	bit;
 
-	i = 128;
-	while (i > 0)
+	while (i--)
 	{
-		if (octet >= i)
-		{
-			write(1, "1", 1);
-			octet %= i;
-			i /= 2; 
-		}
-		else
-		{
-			write(1, "0", 1);
-			i /= 2;
-		}
+		bit = (octet >> i & 1) + '0';
+		write(1, &bit, 1);
 	}
 }
