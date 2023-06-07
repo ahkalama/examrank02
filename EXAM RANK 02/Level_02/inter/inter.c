778b1414f31c1c0e49088d5f9a4d727e4cb56b46
ahkalama <116187665+ahkalama@users.noreply.github.com>
2023-06-07 14:08:32 +0300

    amertkupdateversion2.6

--- EXAM RANK 02/Level_02/inter/inter.c
+++ EXAM RANK 02/Level_02/inter/inter.c
@@ -1,26 +1,37 @@
 #include <unistd.h>
 
-int	iter(char *str, char c, int len)
+int	check(int c, char *str, int index)
 {
 	int i = 0;
-	while(str[i] && (i < len || len == -1))
-		if(str[i++] == c)
-			return 1;
-		return 0;
+	while(i < index)
+	{
+		if(str[i] == c)
+			return 0;
+		i++;
+	}
+	return 1;
 }
 
 int	main(int ac, char **av)
 {
 	int i = 0;
+	int j = 0;
 	if(ac == 3)
 	{
 		while(av[1][i])
 		{
-			if(!iter(av[1], av[1][i], i) && iter(av[2], av[1][i], -1))
-				write(1, &av[1][i], 1);
+			while(av[2][j])
+			{
+				if(av[1][i] == av[2][j] && (check(av[1][i], av[1], i)))
+				{
+					write(1, &av[1][i], 1);
+					break;
+				}
+				j++;
+			}
+			j = 0;
 			i++;
 		}
 	}
 	write(1, "\n", 1);
-	return 0;
 }
