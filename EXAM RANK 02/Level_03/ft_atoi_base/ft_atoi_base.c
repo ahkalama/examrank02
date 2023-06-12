14099a9f0d4b5a49bfb817b1d80bc90d1ee375f6
ahkalama <116187665+ahkalama@users.noreply.github.com>
2023-06-12 22:25:42 +0300

    amertkupdateversion.2.7

--- EXAM RANK 02/Level_03/ft_atoi_base/ft_atoi_base.c
+++ EXAM RANK 02/Level_03/ft_atoi_base/ft_atoi_base.c
@@ -1,28 +1,39 @@
-int		ft_atoi_base(const char *str, int str_base)
-{
-	int i;
-	int n;
-	int len;
-	int result;
+#include <unistd.h>
 
-	i = 0;
-	n = 1;
-	result = 0;
-	if (str[i] == '-')
-	{
-		n = -1;
-		i++;
-	}
-	while (str[i] != '\0')
-	{
-		result *= str_base;
-		if (str[i] >= '0' && str[i] <= '9')
-			result += str[i] - 48;
-		else if (str[i] >= 'A' && str[i] <= 'Z')
-			result += str[i] - 55;
-		else if (str[i] >= 'a' && str[i] <= 'z')
-			result += str[i] - 87;
-		i++;
-	}
-	return (result * n);
+int ft_isvalid(char c, int base)
+{
+    char digits[17] = "0123456789abcdef";
+    char digits2[17] = "0123456789ABCDEF";
+    while (base--)
+        if (digits[base] == c || digits2[base] == c)
+            return (1);
+    return (0);
+}
+
+int ft_value_of(char c)
+{
+    if (c >= '0' && c <= '9')
+        return (c - '0');
+    else if (c >= 'a' && c <= 'f')
+        return (c - 'a' + 10);
+    else if (c >= 'A' && c <= 'F')
+        return (c - 'A' + 10);
+    return (0);
+}
+
+int ft_atoi_base(const char str, int str_base)
+{
+    int i = 0;
+    int result = 0;
+    int sign = 1;
+    while (str[i] <= 32)
+        i++;
+    if(str[i] == '-')
+    {
+        sign = -1;
+        i++;
+    }
+    while (ft_isvalid(str[i], str_base))
+        result = result str_base + ft_value_of(str[i++]);
+    return (result * sign);
 }
