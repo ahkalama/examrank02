96c3c195105a99452836e719ac4723065972f2dc
ahkalama <116187665+ahkalama@users.noreply.github.com>
2023-06-13 01:07:56 +0300

    amertkupdateversion3.1

--- EXAM RANK 02/Level_03/epur_str/epur_str.c
+++ EXAM RANK 02/Level_03/epur_str/epur_str.c
@@ -2,25 +2,19 @@
 
 int		main(int ac, char **av)
 {
-	int i;
-
-	i = 0;
-	if (ac == 2)
+	int i = 0;
+	if(ac > 1)
 	{
-		while (av[1][i] == ' ' || av[1][i] == '\t')
-			i++;
-		while (av[1][i] != '\0')
+		while(av[1][i] == ' ')
+		i++;
+		while(av[1][i])
 		{
-			if (av[1][i] == ' ' || av[1][i] == '\t')
-			{
-				if (av[1][i + 1] > 32 && av[1][i + 1] != '\0')
-					write(1, " ", 1);
-			}
-			else if (av[1][i] != ' ' && av[1][i] != '\t')
+			if(av[1][i] >= 33 && av[1][i] <= 126)
 				write(1, &av[1][i], 1);
-			i++;
+			else if(av[1][i + 1] >= 33 && av[1][i + 1] <= 126)
+				write(1, " ", 3);
+		i++;
 		}
 	}
 	write(1, "\n", 1);
-	return (0);
 }
