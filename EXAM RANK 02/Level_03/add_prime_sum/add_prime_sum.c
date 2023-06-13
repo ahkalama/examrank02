5897be838e91f640fed8ede63f0671c37ede6d48
ahkalama <116187665+ahkalama@users.noreply.github.com>
2023-06-13 18:00:34 +0300

    amertkupdateversion3.0

--- EXAM RANK 02/Level_03/add_prime_sum/add_prime_sum.c
+++ EXAM RANK 02/Level_03/add_prime_sum/add_prime_sum.c
@@ -1,76 +1,50 @@
 #include <unistd.h>
 
-void	ft_putchar(char c)
+int ft_atoi(char *str)
 {
-	write(1, &c, 1);
+    int res = 0;
+    while(*str)
+    {
+        res = res * 10 + *str - '0';
+        str++;
+    }
+    return(res);
 }
 
-void	ft_putnbr(int nb)
+void ft_putnbr(int num)
 {
-	if (nb > 9)
-	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
-	}
-	else
-		ft_putchar(nb + '0');
+    if(num > 9)
+        ft_putnbr(num / 10);
+    write(1, &"0123456789"[num % 10], 1);
 }
 
-int		ft_atoi(char *str)
+int is_prime(int num)
 {
-	int i;
-	int result;
-
-	i = 0;
-	result = 0;
-	while (str[i] != '\0')
-	{
-		result *= 10;
-		result += str[i] - '0';
-		i++;
-	}
-	return (result);
+    if(num <= 1)
+        return(0);
+    else if(num % 2 == 0 && num > 2)
+        return(0);
+    else if(num % 3 == 0 && num > 3)
+        return(0);
+    return(1);
 }
 
-int		ft_is_prime(int nb)
+int main(int argc, char **argv)
 {
-	int i;
-
-	i = 2;
-	while (i < nb)
-	{
-		if (nb % i == 0)
-			return (0);
-		i++;
-	}
-	return (1);
-
-}
-
-int		ft_add_prime_sum(int nb)
-{
-	int i;
-	int prime_sum;
-
-	i = 2;
-	prime_sum = 0;
-	if (nb <= 0)
-		return (0);
-	while (i <= nb)
-	{
-		if (ft_is_prime(i))
-			prime_sum += i;
-		i++;
-	}
-	return (prime_sum);
-}
-
-int		main(int ac, char **av)
-{
-	if (ac == 2)
-		ft_putnbr(ft_add_prime_sum(ft_atoi(av[1])));
+    if(argc == 2)
+    {
+        int num = ft_atoi(argv[1]);
+        int sum = 0;
+        while(num > 0)
+        {
+            if(is_prime(num--))
+            {
+                sum += (num + 1);
+            }
+        }
+        ft_putnbr(sum);
+    }
 	else
 		write(1, "0", 1);
 	write(1, "\n", 1);
-	return (0);
 }
