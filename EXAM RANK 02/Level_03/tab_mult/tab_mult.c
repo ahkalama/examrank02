b582ae1f505a19b30905f64a57d2e2be00cfc957
ahkalama <116187665+ahkalama@users.noreply.github.com>
2023-06-14 01:23:26 +0300

    Update tab_mult.c

--- EXAM RANK 02/Level_03/tab_mult/tab_mult.c
+++ EXAM RANK 02/Level_03/tab_mult/tab_mult.c
@@ -1,31 +1,22 @@
 #include <unistd.h>
-#include <stdlib.h>
 
 int ft_atoi(char *str)
 {
     int res = 0;
     while(*str)
     {
-        res = res * 10 + (*str - 48);
-        str++;   
+        res = res * 10 + (*str - '0');
+        str++;
     }
-    return (res);  
+    return (res);
 }
-void    ft_putchar(char c)
+void    ft_putnbr(int nb)
 {
-    write(1, &c, 1); 
+    if(nb > 9)
+        ft_putnbr(nb / 10);
+    write(1, &"0123456789"[nb % 10], 1);
 }
-void    ft_putnbr(int num)
-{
-    if(num > 9)
-    {
-        ft_putnbr(num / 10);
-        ft_putnbr(num % 10);
-    }
-    else
-        ft_putchar((num % 10) + '0');
-}
-#include "stdio.h"
+
 int main(int ac, char **av)
 {
     int i = 1;
