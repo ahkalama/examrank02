baa5e311590cde47f1c00170e1a137bb2d23c1bb
ahkalama <116187665+ahkalama@users.noreply.github.com>
2023-06-14 01:01:31 +0300

    Update repeat_alpha.c

--- EXAM RANK 02/Level_01/repeat_alpha/repeat_alpha.c
+++ EXAM RANK 02/Level_01/repeat_alpha/repeat_alpha.c
@@ -3,25 +3,22 @@
 int	main(int ac, char **av)
 {
 	int i = 0;
-	int s;
-
-	while(av[1][i])
+	int count = 0;
+	if(ac == 2)
 	{
-		if(av[1][i] >= 'a' && av[1][i] <= 'z' || av[1][i] >= 'A' && av[1][i] <= 'Z')
+		while(av[1][i])
 		{
-			if(av[1][i] >= 'a' && av[1][i] <= 'z')
-			s = av[1][i] - 97;
+			if(av[1][i] >= 'A' && av[1][i] <= 'Z')
+				count = av[1][i] - 64;
+			else if(av[1][i] >= 'a' && av[1][i] <= 'z')
+				count = av[1][i] - 96;
 			else
-			s = av[1][i] - 65;
-			while(s >= 0)
-			{
 				write(1, &av[1][i], 1);
-				s--;
-			}
+			while(count--)
+				write(1, &av[1][i], 1);
+			count = 0;
+			i++;
 		}
-		else
-			write(1, &av[1][i], 1);
-		i++;
 	}
 	write(1, "\n", 1);
 }
