c71e08810642cb41b90d2063b0b7830ad89c8c2b
ahkalama <116187665+ahkalama@users.noreply.github.com>
2023-06-16 18:44:03 +0300

    amertkupdateversion4.2

--- EXAM RANK 02/Level_03/tab_mult/tab_mult.c
+++ EXAM RANK 02/Level_03/tab_mult/tab_mult.c
@@ -17,18 +17,24 @@ void    ft_putnbr(int nb)
     write(1, &"0123456789"[nb % 10], 1);
 }
 
-int main(int ac, char **av)
+int	main(int argc, char *argv[])
 {
-    int i = 1;
-    int num = ft_atoi(av[1]);
-    while(i <= 9)
-    {
-        ft_putnbr(i);
-        write(1, "x", 1);
-        ft_putnbr(num);
-        write(1, "=", 1);
-        ft_putnbr(i * num);
-        write(1, "\n", 1);
-        i++;
-    }
+	int	i = 1;
+	int nbr;
+	if(argc != 2)
+		write(1, "\n", 1);
+	else
+	{
+		nbr = ft_atoi(argv[1]);
+		while (i <= 9)
+		{
+			ft_putnbr(i);
+			write(1, " x ", 3);
+			ft_putnbr(nbr);
+			write(1, " = ", 3);
+			ft_putnbr(i * nbr);
+			write(1, "\n", 1);
+			i++;
+		}
+	}
 }
