70289615e6d170309f44f46ccfb06f3f9de6eaff
ahkalama <116187665+ahkalama@users.noreply.github.com>
2023-06-19 16:52:05 +0300

    Update first_word.c

--- EXAM RANK 02/Level_01/first_word/first_word.c
+++ EXAM RANK 02/Level_01/first_word/first_word.c
@@ -7,7 +7,7 @@ int	main(int ac,char **av)
 	{
 		while(av[1][i] == ' ' || av[1][i] == '\t')
 			i++;
-		while(av[1][i] != '\0' && av[1][i] != ' ' && av[1][i] != '\t')
+		while(av[1][i] && av[1][i] != ' ' && av[1][i] != '\t')
 		{
 			write(1, &av[1][i], 1);
 			i++;
