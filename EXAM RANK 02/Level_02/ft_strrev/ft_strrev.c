d211a88865eb634fbb660b171f06e5ce6ad6c1b3
ahkalama <116187665+ahkalama@users.noreply.github.com>
2023-06-21 13:02:00 +0300

    amertkupdateversion5.1

--- EXAM RANK 02/Level_02/ft_strrev/ft_strrev.c
+++ EXAM RANK 02/Level_02/ft_strrev/ft_strrev.c
@@ -1,22 +1,13 @@
-int		ft_strlen(char *str)
+char    *ft_strrev(char *str)
 {
-	int i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-char	*ft_strrev(char *str)
-{
-	int i;
+	int i = 0;
 	int len;
-	char tmp;
-
+	int tmp;
+	while(str[i])
+		i++;
+	len = i - 1;
 	i = 0;
-	len = ft_strlen(str) - 1;
-	while (len > i)
+	while(len > i)
 	{
 		tmp = str[i];
 		str[i] = str[len];
@@ -24,5 +15,5 @@ char	*ft_strrev(char *str)
 		i++;
 		len--;
 	}
-	return (str);
+	return str;
 }
